416beab1c354a46a48156b02b1834db52f01d9bb
wartman <faxmaster@gmail.com>
2023-11-10 10:31:24 -0600

    ...and now it actually renders

--- src/blok/html/Svg.hx
+++ src/blok/html/Svg.hx
@@ -3,44 +3,54 @@ package blok.html;
 import blok.diffing.Key;
 import blok.html.HtmlEvents;
 import blok.html.SvgAttributes;
-import blok.html.TagCollection;
 import blok.ui.*;
 
 class Svg {
+  @:skip static final tags:Map<String, UniqueId> = [];
+
+  @:skip public static function getTypeForTag(tag:String) {
+    var id = tags.get(tag);
+    if (id == null) {
+      id = new UniqueId();
+      tags.set(tag, id);
+    }
+    return id;
+  }
+
   @:skip macro public static function view(expr);
 
   public static function svg(attributes:SvgAttributes & HtmlEvents & { ?key:Key }, ...children:Child) {
-    static final type = getTypeForTag('svg');
+    var type = getTypeForTag('svg');
     return new VRealNode(type, 'svg', attributes, children.toArray(), attributes.key);
   }
 
   public static function g(attributes:BaseAttr & HtmlEvents & { ?key:Key }, ...children:Child) {
-    static final type = getTypeForTag('g');
+    var type = getTypeForTag('g');
     return new VRealNode(type, 'g', attributes, children.toArray(), attributes.key);
   }
 
   public static function path(attributes:PathAttr & HtmlEvents & { ?key:Key }, ...children:Child) {
-    static final type = getTypeForTag('path');
+    var type = getTypeForTag('path');
     return new VRealNode(type, 'path', attributes, children.toArray(), attributes.key);
   }
 
   public static function polygon(attributes:PolygonAttr & HtmlEvents & { ?key:Key }, ...children:Child) {
-    static final type = getTypeForTag('polygon');
+    var type = getTypeForTag('polygon');
     return new VRealNode(type, 'polygon', attributes, children.toArray(), attributes.key);
   }
 
   public static function circle(attributes:CircleAttr & HtmlEvents & { ?key:Key }, ...children:Child) {
-    static final type = getTypeForTag('circle');
+    var type = getTypeForTag('circle');
     return new VRealNode(type, 'circle', attributes, children.toArray(), attributes.key);
   }
 
   public static function rect(attributes:RectAttr & HtmlEvents & { ?key:Key }, ...children:Child) {
-    static final type = getTypeForTag('rect');
+    var type = getTypeForTag('rect');
     return new VRealNode(type, 'rect', attributes, children.toArray(), attributes.key);
   }
 
   public static function ellipse(attributes:EllipseAttr & HtmlEvents & { ?key:Key }, ...children:Child) {
-    static final type = getTypeForTag('ellipse');
+    var type = getTypeForTag('ellipse');
     return new VRealNode(type, 'ellipse', attributes, children.toArray(), attributes.key);
   }
 }

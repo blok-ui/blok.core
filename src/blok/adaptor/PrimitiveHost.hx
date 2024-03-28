1ff57fcbab9462e916199150f185ffd32c90a5d2
wartman <faxmaster@gmail.com>
2024-03-28 10:07:22 -0500

    Provider updates

--- src/blok/adaptor/PrimitiveHost.hx
+++ src/blok/adaptor/PrimitiveHost.hx
@@ -1,5 +1,3 @@
 package blok.adaptor;
 
-interface PrimitiveHost {
-  // @todo  
-}
+interface PrimitiveHost {}
--- src/blok/context/Provider.hx
+++ src/blok/context/Provider.hx
@@ -1,24 +1,15 @@
 package blok.context;
 
+import blok.context.Providable;
 import blok.ui.*;
 
 class Provider<T:Providable> extends Component {
-  public static function compose(contexts:Array<()->Providable>, child:(context:View)->Child) {
-    var contextFactory = contexts.shift();
-    var component:VNode = Provider.provide(contextFactory, _ -> Scope.wrap(child));
-    while (contexts.length > 0) {
-      var wrapped = component;
-      contextFactory = contexts.shift();
-      component = Provider.provide(contextFactory, _ -> wrapped);
-    }
-    return component;
+  public static function compose(contexts:Array<()->Providable>) {
+    return new VProvider(contexts);
   }
 
-  public inline static function provide<T:Providable>(create:()->T, child:(value:T)->Child) {
-    return node({
-      create: create,
-      child: child
-    });
+  public inline static function provide(create:()->Providable) {
+    return new VProvider([ create ]);
   }
 
   @:attribute final create:()->T;
@@ -48,5 +39,49 @@ class Provider<T:Providable> extends Component {
       context = newContext;
     }
     return child(context);
+  }
+}
+
+abstract VProvider({
+  public final contexts:Array<()->Providable>;
+  public var child:Null<(context:View)->Child>;
+}) {
+  public inline function new(contexts) {
+    this = {
+      contexts: contexts,
+      child: null
+    };
+  }
+
+  public inline function provide(value) {
+    this.contexts.push(value);
+    return abstract;
+  }
+
+  public inline function child(child:(context:View)->Child) {
+    this.child = child;
+    return abstract;
+  }
+
+  @:to
+  public function node():Child {
+    var contexts = this.contexts.copy();
+    var child = this.child;
+    var contextFactory = contexts.shift();
+    var component:VNode = Provider.node({
+      create: contextFactory,
+      child: _ -> Scope.wrap(child)
+    });
+
+    while (contexts.length > 0) {
+      var wrapped = component;
+      contextFactory = contexts.shift();
+      component = Provider.node({
+        create: contextFactory,
+        child: _ -> wrapped
+      });
+    }
+
+    return component;
   }
 }
